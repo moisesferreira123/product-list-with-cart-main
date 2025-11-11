b3d000732c1eb80752679edfea7c0b07d3ec6924
Moisés Ferreira <moisesaoaoao@gmail.com>
2025-11-10 22:46:40 -0300

    Implementando o Your Cart

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let desserts = [];
 let cart = {};
 let qtdCart = 0;
 const gridDesserts = document.getElementById("grid-desserts");
-const yourCart = querySelector(".your-cart");
+const yourCart = document.getElementById("your-cart");
 
 async function getData() {
   const data = await fetch("data.json")
@@ -57,9 +57,42 @@ function showYourCart() {
       <p class="text-xs text-(--rose500) font-medium text-center">Your added items will appear here</p>
     `;
   } else {
-    const html = cart.map(dessert => {
-      
+    const html = Object.entries(cart).map(([dessertId, qtd]) => {
+      return `
+        <div class="flex justify-between items-center">
+          <div class="space-y-1">
+            <h3 class="text-sm text-(--rose900) font-medium">${desserts[dessertId].name}</h3>
+            <div class="flex gap-4">
+              <p class="text-(--red) font-medium text-sm">${qtd}<span class="text-xs">x</span></p>
+              <div class="flex gap-2 text-sm">
+                <p class="text-(--rose400)"><span class="text-xs">@</span>$${desserts[dessertId].price}</p>
+                <p class="text-(--rose500)">$${(qtd*desserts[dessertId].price).toFixed(2)}</p>
+              </div>
+            </div>
+          </div>
+          <button class="text-(--rose300) hover:text-(--rose900) hover:cursor-pointer"><svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-x-icon lucide-circle-x"><circle cx="12" cy="12" r="7"/><path d="m15 9-6 6"/><path d="m9 9 6 6"/></svg></button>
+        </div>
+        <div class="border-t border-(--rose100)"></div>
+      `;
     }).join("");
+    yourCart.innerHTML = `
+      <h2 class="font-bold text-xl text-(--red)">Your Cart (0)</h2>
+      <div class="space-y-4"> 
+    ` 
+    + html +
+    // TODO: Calcular preço total
+    `
+      </div>
+      <div class="flex justify-between items-center">
+        <p class="text-sm text-(--rose900)">Order Total</p>
+        <p class="text-3xl font-bold text-(--rose900)">$${}</p>
+      </div>
+      <div class="flex justify-center text-center gap-2 bg-(--rose50) p-4 rounded-md">
+        <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 21 20"><path fill="#1EA575" d="M8 18.75H6.125V17.5H8V9.729L5.803 8.41l.644-1.072 2.196 1.318a1.256 1.256 0 0 1 .607 1.072V17.5A1.25 1.25 0 0 1 8 18.75Z"/><path fill="#1EA575" d="M14.25 18.75h-1.875a1.25 1.25 0 0 1-1.25-1.25v-6.875h3.75a2.498 2.498 0 0 0 2.488-2.747 2.594 2.594 0 0 0-2.622-2.253h-.99l-.11-.487C13.283 3.56 11.769 2.5 9.875 2.5a3.762 3.762 0 0 0-3.4 2.179l-.194.417-.54-.072A1.876 1.876 0 0 0 5.5 5a2.5 2.5 0 1 0 0 5v1.25a3.75 3.75 0 0 1 0-7.5h.05a5.019 5.019 0 0 1 4.325-2.5c2.3 0 4.182 1.236 4.845 3.125h.02a3.852 3.852 0 0 1 3.868 3.384 3.75 3.75 0 0 1-3.733 4.116h-2.5V17.5h1.875v1.25Z"/></svg>
+        <p class="text-sm text-(--rose900)">This is a <span class="font-semibold">carbon-neutral</span> delivery</p>
+      </div>
+      <button class="w-full bg-(--red) text-(--rose50) p-3.5 rounded-full text-sm font-medium hover:cursor-pointer hover:bg-(--red-dark)">Confirm Order</button>
+    `;
   }
 }
 
