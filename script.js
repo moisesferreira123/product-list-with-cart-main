0251b97f0392d1774c40ec8423e56c4619da2691
Moisés Ferreira <moisesaoaoao@gmail.com>
2025-11-25 16:29:56 -0300

    Modal de confirmação de pedido concluído

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let cart = {};
 let qtdCart = 0;
 const gridDesserts = document.getElementById("grid-desserts");
 const yourCart = document.getElementById("your-cart");
+const overlay = document.getElementById("overlay");
+const cartOrderConfirmed = document.getElementById("cart-order-confirmed");
 
 async function getData() {
   const data = await fetch("data.json")
@@ -57,6 +59,16 @@ function priceTotal() {
   return priceTotal;
 }
 
+function startNewOrder() {
+  for(const dessertId in cart) {
+    const gridButton = document.querySelector('#dessert-' + dessertId + ' .grid-button');
+    gridButton.outerHTML = buttonAddToCard();
+    delete cart[dessertId];
+  }
+  qtdCart = 0;
+  showYourCart();
+}
+
 function showYourCart() {
   if(Object.keys(cart).length === 0) {
     yourCart.innerHTML = `
@@ -99,9 +111,37 @@ function showYourCart() {
         <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 21 20"><path fill="#1EA575" d="M8 18.75H6.125V17.5H8V9.729L5.803 8.41l.644-1.072 2.196 1.318a1.256 1.256 0 0 1 .607 1.072V17.5A1.25 1.25 0 0 1 8 18.75Z"/><path fill="#1EA575" d="M14.25 18.75h-1.875a1.25 1.25 0 0 1-1.25-1.25v-6.875h3.75a2.498 2.498 0 0 0 2.488-2.747 2.594 2.594 0 0 0-2.622-2.253h-.99l-.11-.487C13.283 3.56 11.769 2.5 9.875 2.5a3.762 3.762 0 0 0-3.4 2.179l-.194.417-.54-.072A1.876 1.876 0 0 0 5.5 5a2.5 2.5 0 1 0 0 5v1.25a3.75 3.75 0 0 1 0-7.5h.05a5.019 5.019 0 0 1 4.325-2.5c2.3 0 4.182 1.236 4.845 3.125h.02a3.852 3.852 0 0 1 3.868 3.384 3.75 3.75 0 0 1-3.733 4.116h-2.5V17.5h1.875v1.25Z"/></svg>
         <p class="text-sm text-(--rose900)">This is a <span class="font-semibold">carbon-neutral</span> delivery</p>
       </div>
-      <button class="w-full bg-(--red) text-(--rose50) p-3.5 rounded-full text-sm font-medium hover:cursor-pointer hover:bg-(--red-dark)">Confirm Order</button>
+      <button id="confirm-order" class="w-full bg-(--red) text-(--rose50) p-3.5 rounded-full text-sm font-medium hover:cursor-pointer hover:bg-(--red-dark)">Confirm Order</button>
     `;
   }
+}
+
+function addCartInOrderConfirmed() {
+  const html = Object.entries(cart).map(([dessertId, qtd]) => {
+    if(!desserts[dessertId]) return;
+    return `
+      <div class="flex justify-between items-center p-4">
+        <div class="flex items-center gap-3">
+          <img src="${desserts[dessertId].image.thumbnail}" alt="dessert's image" class="h-12 w-12 rounded-sm">
+          <div class="space-y-1">
+            <h3 class="text-sm text-(--rose900) font-medium">${desserts[dessertId].name}</h3>
+            <div class="flex gap-4">
+              <p class="text-(--red) font-medium text-sm">${qtd}<span class="text-xs">x</span></p>
+              <p class="text-(--rose400)"><span class="text-xs">@</span>$${(qtd*desserts[dessertId].price).toFixed(2)}</p>
+            </div>
+          </div>
+        </div>
+        <p class="text-(--rose900) font-medium">$${(qtd*desserts[dessertId].price).toFixed(2)}</p>
+      </div>
+      <div class="border-t border-(--rose100)"></div>
+    `;
+  }).join("");
+  cartOrderConfirmed.innerHTML = html + `
+    <div class="flex justify-between items-center p-4 pb-5 pt-5">
+      <p class="text-xs text-(--rose900)">Order Total</p>
+      <p class="text-xl text-(--rose900) font-bold">$${priceTotal().toFixed(2)}</p>
+    </div>
+  `;
 }
 
 gridDesserts.addEventListener('click', event => {
@@ -143,7 +183,6 @@ gridDesserts.addEventListener('click', event => {
   }
 });
 
-// TODO: Ajeitar bug nos desserts ao apagar sobremesa do carrinho
 yourCart.addEventListener('click', event => {
   const button = event.target.closest('button');
   if(!button) return;
@@ -156,6 +195,22 @@ yourCart.addEventListener('click', event => {
     gridButton.outerHTML = buttonAddToCard();
     delete cart[dessertCartId];
     showYourCart();
+  }
+
+  const confirmOrderButton = button.closest('#confirm-order');
+  if(confirmOrderButton) {
+    overlay.classList.remove('hidden');
+    overlay.classList.add('flex');
+    addCartInOrderConfirmed();
+  }
+});
+
+document.addEventListener('click', event => {
+  const startNewOrderButton = event.target.closest('#start-new-order');
+  if(startNewOrderButton) {
+    overlay.classList.remove('flex');
+    overlay.classList.add('hidden');
+    startNewOrder();
   }
 });
 
